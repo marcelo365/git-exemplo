3bcb1f21325c03c81f73e03d6a0b2cc61d84ed53
marcelo365 <marcelo365@gmail.com>
2024-05-06 08:47:27 +0100

    quinta versão

--- vector.js
+++ vector.js
@@ -2,8 +2,7 @@ var a = [3 , 4 , 6];
 
 function somarElementosVector(a){
     var res = 0;
-    var b = 0;
-    for ( b=0 ; b < a.length ; b++){
+    for ( var b=0 ; b < a.length ; b++){
         res += a[b];
     }
     console.log(res)
